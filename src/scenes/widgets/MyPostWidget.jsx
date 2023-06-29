71fb916920a6cfb8feb97fd01e3c7eac1845fd95
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-06-29 09:40:40 +0300

    fix: update server link

--- src/scenes/widgets/MyPostWidget.jsx
+++ src/scenes/widgets/MyPostWidget.jsx
@@ -38,7 +38,7 @@ const MyPostWidget = ({ picturePath }) => {
         formData.append("picturePath", image.name);
       }
   
-      const response = await fetch(`http://localhost:3001/posts`, {
+      const response = await fetch(`https://sociopedia-backend-9md6.onrender.com/posts`, {
         method: "POST",
         headers: { Authorization: `Bearer ${token}` },
         body: formData,
